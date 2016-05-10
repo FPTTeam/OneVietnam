d4258daf62ec625a2fc55611a9c3d859001b1703
ThamDTH <truongthiphuongdu@gmail.com>
2016-05-10 19:45:36 +0700

    ThamDTH

---
+++ DAL/CountryDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entities;
+using MongoDB.Driver;
+
+namespace DAL
+{
+    public class CountryDAO
+    {
+        private readonly MongoService<User> _repository;
+    }
+}
